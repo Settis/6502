3ca29b5d3650352fb7de9057c5c6cd6cb8aa7f61
Anton <strannik.anton@gmail.com>
2021-03-10 17:40:40 +0100

    Display refactored to use port handshake

--- hard/prog/display.asm
+++ hard/prog/display.asm
@@ -4,6 +4,8 @@ REG_B = $8000
 REG_A = $8001
 DDRB = $8002
 DDRA = $8003
+PCR = $800C
+CA_CONTROL = %00001010
 
 EN = %00000001
 RS = %00000010
@@ -71,7 +73,6 @@ cmd:
     TAX
     AND #$F0
     STA REG_A
-    JSR tick
     TXA
     ROL
     ROL
@@ -79,7 +80,6 @@ cmd:
     ROL
     AND #$F0
     STA REG_A
-    JSR tick
     RTS
 
 dataTick:
@@ -97,35 +97,35 @@ dataTick:
 data:
     TAX
     AND #$F0
+    ORA #$01
     STA REG_A
-    JSR dataTick
     TXA
     ROL
     ROL
     ROL
     ROL
     AND #$F0
+    ORA #$01
     STA REG_A
-    JSR dataTick
     RTS
 
+hello_srt:
+    DC "WDC 6502", 0
+
 start:
-    LDA #$F0
+    LDA #$F1
     STA DDRA
 
-    LDA #$03
-    STA DDRB
+    LDA #CA_CONTROL
+    STA PCR
 
     LDA #%00110000
     STA REG_A
-
-    JSR tick
-    JSR tick
-    JSR tick
+    STA REG_A
+    STA REG_A
 
     LDA #%00100000
     STA REG_A
-    JSR tick
 
     LDA #%00100000
     JSR cmd
