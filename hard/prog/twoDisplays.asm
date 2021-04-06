530cf22c7025ce785b9b5392cc6ceba11aafea10
Anton <strannik.anton@gmail.com>
2021-04-06 22:17:00 +0200

    Rewrite twoDisplays for right PortB pinout

--- hard/prog/twoDisplays.asm
+++ hard/prog/twoDisplays.asm
@@ -6,80 +6,7 @@ home_made_srt:
     DC "Home made comp", 0
 
 wdc_srt:
-    ; DC "WDC 6502", 0
-    DC %11101010
-    DC %00100010
-    DC %11000010
-    DC %00000100
-    DC %01101100
-    DC %10101100
-    DC %00001100
-    DC %01001100
-    DC 0
-
-INIT_DISPLAY_2:
-    LDX #$0
-
-; 3 times first part of 8-bit mode
-    LDA #%00000011
-    STA (DISPLAY_ADDR,X)
-    STA (DISPLAY_ADDR,X)
-    STA (DISPLAY_ADDR,X)
-
-; first part of 4-bit mode
-    LDA #%00000100
-    STA (DISPLAY_ADDR,X)
-
-; 4-bit mode command
-    LDA #%00000100
-    STA (DISPLAY_ADDR,X)
-    LDA #%00000000
-    STA (DISPLAY_ADDR,X)
-
-; Display ON
-    LDA #%00000000
-    STA (DISPLAY_ADDR,X)
-    LDA #%00000111
-    STA (DISPLAY_ADDR,X)
-
-; Clear display
-    LDA #%00000000
-    STA (DISPLAY_ADDR,X)
-    LDA #%00001000
-    STA (DISPLAY_ADDR,X)
-
-; Entry mode set
-    LDA #%00000000
-    STA (DISPLAY_ADDR,X)
-    LDA #%00000110
-    STA (DISPLAY_ADDR,X)
-
-    RTS
-
-PRINT_STRING_2:
-    LDY #$0
-    LDX #$0
-
-PS_LOOP_2:
-    LDA (DISPLAY_STRING_ADDR),Y
-    BEQ PS_END_2
-    STA DISPLAY_TMP
-    AND #$0F
-    ORA #$80
-    STA (DISPLAY_ADDR,X)
-    LDA DISPLAY_TMP
-    ROR
-    ROR
-    ROR
-    ROR
-    AND #$0F
-    ORA #$80
-    STA (DISPLAY_ADDR,X)
-    INY
-    JMP PS_LOOP_2
-
-PS_END_2:
-    RTS
+    DC "WDC 6502", 0
 
 start:
 ; Disable all interrupts
@@ -89,7 +16,6 @@ start:
 ; Setup port directions
     LDA #$F3
     STA VIA_FIRST_DDRA
-    LDA #$CF
     STA VIA_FIRST_DDRB
 
 ; Setup handshakes
@@ -102,7 +28,7 @@ start:
 
 ; Init display 2
     WRITE_WORD VIA_FIRST_RB, DISPLAY_ADDR
-    JSR INIT_DISPLAY_2
+    JSR INIT_DISPLAY
 
     WRITE_WORD VIA_FIRST_RA, DISPLAY_ADDR
     WRITE_WORD home_made_srt, DISPLAY_STRING_ADDR
@@ -110,6 +36,6 @@ start:
 
     WRITE_WORD VIA_FIRST_RB, DISPLAY_ADDR
     WRITE_WORD wdc_srt, DISPLAY_STRING_ADDR
-    JSR PRINT_STRING_2   
+    JSR PRINT_STRING   
     
     RESET_VECTOR start, start, start
