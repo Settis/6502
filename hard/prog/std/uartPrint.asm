23f04150c12c4de34f6447f7a6ce661c45268daf
strannik <strannik.anton@gmail.com>
2024-01-27 23:15:29 +0100

    Rewriting uart test

--- hard/prog/std/uartPrint.asm
+++ hard/prog/std/uartPrint.asm
@@ -30,29 +30,29 @@ main:
 */
 
 ; You can change buffer size and place if needed
-OUTPUT_BUFFER_AND_MASK set $FF
+_OUTPUT_BUFFER_AND_MASK set $FF
 
-INTERRUPT_INDIRECT = $FE ; $ $FF
-BUFFER_TIMER_FLAG = $01
+_INTERRUPT_INDIRECT = $FE ; $ $FF
+_BUFFER_TIMER_FLAG = $01
 
     SEG.U zpVars 
-BUFFER_READ_IND: DS 1
-BUFFER_WRITE_IND: DS 1
-BUFFER_FLAG: DS 1
-STRING_POINTER: DS 2
+_BUFFER_READ_IND: DS 1
+_BUFFER_WRITE_IND: DS 1
+_BUFFER_FLAG: DS 1
+_STRING_POINTER: DS 2
 
     SEG.U upperRam
 
-OUTPUT_BUFFER: DS OUTPUT_BUFFER_AND_MASK + 1
+_OUTPUT_BUFFER: DS _OUTPUT_BUFFER_AND_MASK + 1
 
     SEG code
 
 INIT_UART_PRINT:
     LDA #0
-    STA BUFFER_READ_IND
-    STA BUFFER_WRITE_IND
-    STA BUFFER_FLAG
-    WRITE_WORD UART_PRINT_INTERRUPT, INTERRUPT_INDIRECT
+    STA _BUFFER_READ_IND
+    STA _BUFFER_WRITE_IND
+    STA _BUFFER_FLAG
+    WRITE_WORD _UART_PRINT_INTERRUPT, _INTERRUPT_INDIRECT
     ; Enable VIA T2 interrupt
     LDA #$A0
     STA VIA_FIRST_IER
@@ -67,24 +67,24 @@ INIT_UART_PRINT:
     ENDM
 
     MAC UART_PRINT_STRING
-        WRITE_WORD {1}, STRING_POINTER
-        JSR UART_PRINT_STRING_SUB
+        WRITE_WORD {1}, _STRING_POINTER
+        JSR _UART_PRINT_STRING_SUB
     ENDM
 
     MAC UART_PRINTLN
         LDA #$A ; new line ASCII
-        JSR write_to_uart
+        JSR _write_to_uart
     ENDM
 
-UART_PRINT_STRING_SUB:
+_UART_PRINT_STRING_SUB:
     TYA
     PHA
     subroutine
     LDY #0
 .loop:
-    LDA (STRING_POINTER),Y
+    LDA (_STRING_POINTER),Y
     BEQ .end
-    JSR write_to_uart
+    JSR _write_to_uart
     INY
     JMP .loop
 .end:
@@ -94,18 +94,18 @@ UART_PRINT_STRING_SUB:
 
 UART_PRINT_NUMBER:
     subroutine
-    STA STRING_POINTER
+    STA _STRING_POINTER
     LSR
     LSR
     LSR
     LSR
-    JSR uart_print_half_humber
-    LDA STRING_POINTER
+    JSR _uart_print_half_humber
+    LDA _STRING_POINTER
     AND #$F
-    JSR uart_print_half_humber
+    JSR _uart_print_half_humber
     RTS
 
-uart_print_half_humber:
+_uart_print_half_humber:
     subroutine
     CMP #$A
     BCC .digit
@@ -114,31 +114,31 @@ uart_print_half_humber:
 .digit:
     ADC #$30 ; '0' ASCII character
 .end:
-    JSR write_to_uart
+    JSR _write_to_uart
     RTS
 
-UART_PRINT_INTERRUPT:
+_UART_PRINT_INTERRUPT:
     subroutine
     PHA
     TXA
     PHA
 
-    LDX BUFFER_READ_IND
-    CPX BUFFER_WRITE_IND
+    LDX _BUFFER_READ_IND
+    CPX _BUFFER_WRITE_IND
     BEQ .buffer_end
-    LDA OUTPUT_BUFFER,X
+    LDA _OUTPUT_BUFFER,X
     STA UART_DATA_REG
     INX
     TXA
-    AND #OUTPUT_BUFFER_AND_MASK
-    STA BUFFER_READ_IND
+    AND #_OUTPUT_BUFFER_AND_MASK
+    STA _BUFFER_READ_IND
     
-    JSR set_timer
+    JSR _set_timer
     JMP .interrupt_end
 .buffer_end:
     LDA VIA_FIRST_T2C_L
     LDA #0
-    STA BUFFER_FLAG
+    STA _BUFFER_FLAG
 .interrupt_end
     PLA
     TAX
@@ -146,48 +146,48 @@ UART_PRINT_INTERRUPT:
     RTI
 
 UART_PRINT_WAIT_FOR_BUFFER:
-    LDA BUFFER_FLAG
+    LDA _BUFFER_FLAG
     BNE UART_PRINT_WAIT_FOR_BUFFER
     RTS
 
 ; try to write A to UART
 ; it UART is busy then save it into buffer
-write_to_uart:
+_write_to_uart:
     PHA
     ; Check if timer in action
     SEI
-    LDA BUFFER_FLAG
-    BNE write_to_buffer
+    LDA _BUFFER_FLAG
+    BNE _write_to_buffer
     ; if not, write to uart and set the timer
     CLI
     PLA
     STA UART_DATA_REG
-    LDA #BUFFER_TIMER_FLAG
-    STA BUFFER_FLAG
-    JSR set_timer
+    LDA #_BUFFER_TIMER_FLAG
+    STA _BUFFER_FLAG
+    JSR _set_timer
     RTS
     
     ; if yes, write to buffer if can
     ; if can't go to endlress loop
-write_to_buffer:
+_write_to_buffer:
     subroutine
-    LDX BUFFER_WRITE_IND
+    LDX _BUFFER_WRITE_IND
     PLA
-    STA OUTPUT_BUFFER,X
+    STA _OUTPUT_BUFFER,X
     INX
     TXA
-    AND #OUTPUT_BUFFER_AND_MASK
+    AND #_OUTPUT_BUFFER_AND_MASK
 .wait_loop:
-    CMP BUFFER_READ_IND
+    CMP _BUFFER_READ_IND
     BEQ .clear_interrupt_and_wait
-    STA BUFFER_WRITE_IND
+    STA _BUFFER_WRITE_IND
     CLI
     RTS
 .clear_interrupt_and_wait:
     CLI
     JMP .wait_loop
 
-set_timer:
+_set_timer:
     ; 6M / 9600 * 11 = $1ADB
     LDA #$DB
     STA VIA_FIRST_T2C_L
