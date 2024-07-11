5a3bb0c9a1fbd9718b30a8e5f094237fcab688a3
Anton <strannik.anton@gmail.com>
2024-07-11 22:47:39 +0200

    Simple commands

--- figForth/prog/classic.asm
+++ figForth/prog/classic.asm
@@ -34,6 +34,8 @@
 ; And finally `;S` and
 ; `EXECUTE` for execution.
 
+; Boolean flags: 0 - false, non-zero - true
+
 ;    Dictionary record:
 ; NFA |1|P|S|Length|   p - immediate flag, s - smudge flag
 ;     |0| ASCII #0 |
@@ -52,17 +54,15 @@ S0_CONST = $7DFE
 TIB_CONST = $7E00
 R0_CONST = $7EFE
 
-SPACE_ASCII = $20
-ZERO_ASCII = $30
-CAPITAL_A_ASCII = $41
-
     seg.u UserArea
     org $7F00
     ; It's empty for now
 
     seg.u zp
     org 0
-TMP:     ds 2 ; [internal] for transfering data between stacks
+STACK_TMP: ds 2 ; [internal] for transfering data between stacks
+INTERNAL_TMP: ds 2 ; [internal]
+TMP_LENGTH: ds 1 ; [internal] for string length
 DP_ADDR: ds 2
 SP_ADDR: ds 2
 IN_ADDR: ds 2
@@ -70,6 +70,7 @@ RP_ADDR: ds 2
 IP_ADDR: ds 2 ; [internal] interpretive pointer for call executor
 W_ADDR:  ds 2 ; [internal] current word pointer for call executor
 TEXT_ADDR: ds 2 ; [internal] pointer for initial text interpreter
+CONTEXT_VALUE: ds 2
 
     seg CODE
     org $C000
@@ -80,6 +81,12 @@ TEXT_ADDR: ds 2 ; [internal] pointer for initial text interpreter
         LDA #>{1}
         STA {2}+1
     ENDM
+    MAC COPY_WORD_TO ; 1 - word addr, 2 - dst addr
+        LDA {1}
+        STA {2}
+        LDA {1}+1
+        STA {2}+1
+    ENDM
 
 START:
     ; init
@@ -87,59 +94,183 @@ START:
     WRITE_WORD_TO S0_CONST, SP_ADDR
     WRITE_WORD_TO TIB_CONST, IN_ADDR
     WRITE_WORD_TO R0_CONST, RP_ADDR
+    WRITE_WORD_TO LAST_F_WORD, CONTEXT_VALUE
 
     WRITE_WORD_TO TEXT, TEXT_ADDR
 TEXT_LOOP:
+    JSR COPY_WORD_FROM_TEXT
+
+    COPY_WORD_TO DP_ADDR, STACK_TMP
+    JSR PUSH_TO_S
+    COPY_WORD_TO CONTEXT_VALUE, STACK_TMP
+    JSR PUSH_TO_S
+    JSR FIND_IN_DICTIONARY
+
+    JSR PULL_FROM_S
+    LDA STACK_TMP
+    BEQ .parseNumber
+
+    JSR PULL_FROM_S ; throw size
+    JSR PULL_FROM_S ; pull PFA
+    SEC
+    LDA STACK_TMP
+    SBC #2
+    STA STACK_TMP
+    LDA STACK_TMP+1
+    SBC #0
+    STA STACK_TMP+1
+    JSR PUSH_TO_S  ; convert PFA to CFA
+
+    JSR EXECUTE
+    JMP .endTextLoop
+
+.parseNumber:
     JSR READ_NUMBER
+.endTextLoop:
+    ; check if it's end of the text
     LDY #0
     LDA (TEXT_ADDR),Y
     BNE TEXT_LOOP
 
+    JSR PRINT_STACK
     BRK
+
+COPY_WORD_FROM_TEXT:
+    SUBROUTINE
+    COPY_WORD_TO DP_ADDR, STACK_TMP
+    INC STACK_TMP
+    BNE .skipUp
+    INC STACK_TMP+1
+.skipUp:
+    LDY #0
+.copyLoop:
+    LDA (TEXT_ADDR),Y
+    CMP #'   ; check for space
+    BEQ .end
+    STA (STACK_TMP),Y
+    INY
+    JMP .copyLoop
+.end:
+    LDX #0
+    TYA
+    STA (DP_ADDR,X)
+    SEC ; For increase text pointer by word size +1 for space
+    ADC TEXT_ADDR
+    STA TEXT_ADDR
+    LDA TEXT_ADDR+1
+    ADC #0
+    STA TEXT_ADDR+1
+    RTS
+
+FIND_IN_DICTIONARY: ; (FIND) primitive ( NAME_ADDR DICTIONARY_RECORD_ADDR -- PFA NAME_LENGTH TF / FF )
+    SUBROUTINE
+    JSR PULL_FROM_S
+    COPY_WORD_TO STACK_TMP, INTERNAL_TMP ; INTERNAL_TMP for distionary record addr
+    JSR PULL_FROM_S                      ; STACK_TMP for name addr
+    LDY #0
+    LDA (STACK_TMP),Y
+    CMP #32
+    BCC .normalSize
+    LDA #31  ; the maximum name size
+.normalSize:
+    STA TMP_LENGTH
+.checkName:
+    LDY #0
+    LDA (INTERNAL_TMP),Y
+    AND #$1F  ; to extract only size
+    CMP TMP_LENGTH
+    BNE .nextRecord
+.nextChar:
+    INY
+    LDA (INTERNAL_TMP),Y
+    AND #$7F  ; to clear high bit
+    CMP (STACK_TMP),Y
+    BNE .nextRecord
+    CPY TMP_LENGTH
+    BNE .nextChar
+    ; We found it!
+    TYA
+    CLC
+    ADC #[1+2+2] ; 1 for point to the LFA, 2 for CFA, 2 for PFA
+    ADC INTERNAL_TMP
+    STA STACK_TMP
+    LDA INTERNAL_TMP+1
+    ADC #0
+    STA STACK_TMP+1
+    JSR PUSH_TO_S
+    LDA TMP_LENGTH
+    STA STACK_TMP
+    LDA #0
+    STA STACK_TMP+1
+    JSR PUSH_TO_S
+    JMP PUSH_TRUE_TO_S 
+
+.nextRecord:
+    LDY #0
+    LDA (INTERNAL_TMP),Y
+    AND #$1F  ; to extract only size
+    TAY
+    INY
+    ; Reading LFA to X and A
+    LDA (INTERNAL_TMP),Y
+    TAX
+    INY 
+    LDA (INTERNAL_TMP),Y
+    BEQ .notFound ; if upper byte of address is 0 I don't need to check the lover part
+    ; Store LFA to INTERNAL_TMP, go to the next record
+    STA INTERNAL_TMP+1
+    TXA
+    STA INTERNAL_TMP
+    JMP .checkName
+.notFound:
+    JSR PUSH_FALSE_TO_S
+    RTS
 
 READ_NUMBER:
     SUBROUTINE
     LDY #0
-    STY TMP
-    STY TMP+1
+    LDA (DP_ADDR),Y
+    STA TMP_LENGTH ; Store size here
+    STY STACK_TMP
+    STY STACK_TMP+1
 .digitLoop:
-    LDA (TEXT_ADDR),Y ; read next character
-    INC TEXT_ADDR     ; increase pointer
-    BNE .skipUp
-    INC TEXT_ADDR+1
-.skipUp:
-    CMP #SPACE_ASCII
-    BNE .proceed
-    JMP PUSH_TO_S
-.proceed
+    INY
+    LDA (DP_ADDR),Y 
     ; shift TMP
     LDX #4
 .shifting:
-    ASL TMP
-    ROL TMP+1
+    ASL STACK_TMP
+    ROL STACK_TMP+1
     DEX
     BNE .shifting
     ; add number
     SEC
-    SBC #ZERO_ASCII
+    SBC #'0
     CMP #10
     BCC .digitChar
     ; carry is set already
-    SBC #7 ; Convert ABC to hex
+    SBC #['A-'9-1]
 .digitChar:
-    ORA TMP
-    STA TMP
-    JMP .digitLoop
+    ORA STACK_TMP
+    STA STACK_TMP
+    CPY TMP_LENGTH ; check size
+    BNE .digitLoop
+    JMP PUSH_TO_S
 
+EXECUTE:  ; FORTH ( CFA -- )
+    SUBROUTINE
+    JSR F_WORD_READ_FROM_ADDR_CODE
+    JSR PULL_FROM_S
+    JMP (STACK_TMP)
 
 ; Transfering data between stacks goes via TMP 
 PUSH_TO_S:
     SUBROUTINE
     LDY #$0
-    LDA TMP
+    LDA STACK_TMP
     STA (SP_ADDR),Y
     INY
-    LDA TMP+1
+    LDA STACK_TMP+1
     STA (SP_ADDR),Y
 
     SEC
@@ -163,18 +294,18 @@ PULL_FROM_S:
 
     LDY #$0
     LDA (SP_ADDR),Y
-    STA TMP
+    STA STACK_TMP
     INY
     LDA (SP_ADDR),Y
-    STA TMP+1
+    STA STACK_TMP+1
     RTS
 
 PUSH_TO_R:
     LDY #$0
-    LDA TMP
+    LDA STACK_TMP
     STA (RP_ADDR),Y
     INY
-    LDA TMP+1
+    LDA STACK_TMP+1
     STA (RP_ADDR),Y
     DEC RP_ADDR ; return stack will fit in one page, I don't care about upper byte
     DEC RP_ADDR
@@ -185,18 +316,230 @@ PULL_FROM_R:
     INC RP_ADDR
     LDY #$0
     LDA (RP_ADDR),Y
-    STA TMP
+    STA STACK_TMP
     INY
     LDA (RP_ADDR),Y
-    STA TMP+1
+    STA STACK_TMP+1
     RTS
+
+PUSH_TRUE_TO_S:
+    LDA #$FF
+    STA STACK_TMP
+    STA STACK_TMP+1
+    JMP PUSH_TO_S
+
+PUSH_FALSE_TO_S:
+    LDA #$00
+    STA STACK_TMP
+    STA STACK_TMP+1
+    JMP PUSH_TO_S
+
+;  ======= Forth words ========    
+F_WORD_TO_RETURN_STACK: ; >R
+    DC 2  | $80
+    DC '>
+    DC 'R | $80
+    DC.W 0
+    DC.W F_WORD_TO_RETURN_STACK_CODE
+F_WORD_TO_RETURN_STACK_CODE:    
+    JSR PULL_FROM_S
+    JMP PUSH_TO_R
+
+F_WORD_FROM_RETURN_STACK: ; R>
+    DC 2  | $80
+    DC 'R
+    DC '> | $80
+    DC.W F_WORD_TO_RETURN_STACK
+    DC.W F_WORD_FROM_RETURN_STACK_CODE
+F_WORD_FROM_RETURN_STACK_CODE:
+    JSR PULL_FROM_R
+    JMP PUSH_TO_S
+
+F_WORD_READ_FROM_ADDR: ; @
+    DC 1   | $80
+    DC '@  | $80
+    DC.W F_WORD_FROM_RETURN_STACK
+    DC.W F_WORD_READ_FROM_ADDR_CODE
+F_WORD_READ_FROM_ADDR_CODE:
+    JSR PULL_FROM_S
+    LDY #0
+    LDA (STACK_TMP),Y
+    TAX
+    INY
+    LDA (STACK_TMP),Y
+    STA STACK_TMP+1
+    TXA
+    STA STACK_TMP
+    JMP PUSH_TO_S
+
+F_WORD_WRITE_TO_ADDR: ; !
+    DC 1  | $80
+    DC '! | $80
+    DC.W F_WORD_READ_FROM_ADDR
+    DC.W F_WORD_WRITE_TO_ADDR_CODE
+F_WORD_WRITE_TO_ADDR_CODE:
+    JSR PULL_FROM_S
+    COPY_WORD_TO STACK_TMP, INTERNAL_TMP
+    JSR PULL_FROM_S
+    LDY #0
+    LDA STACK_TMP
+    STA (INTERNAL_TMP),Y
+    INY
+    LDA STACK_TMP+1
+    STA (INTERNAL_TMP),Y
+    RTS
+
+F_WORD_PLUS: ; +
+    DC 1  | $80
+    DC '+ | $80
+    DC.W F_WORD_WRITE_TO_ADDR
+    DC.W F_WORD_PLUS_CODE
+F_WORD_PLUS_CODE:
+    JSR PULL_FROM_S
+    COPY_WORD_TO STACK_TMP, INTERNAL_TMP
+    JSR PULL_FROM_S
+    CLC
+    LDA INTERNAL_TMP
+    ADC STACK_TMP
+    STA STACK_TMP
+    LDA INTERNAL_TMP+1
+    ADC STACK_TMP+1
+    STA STACK_TMP+1
+    JMP PUSH_TO_S
+
+F_WORD_MUL_BY_2: ; 2*
+    DC 2  | $80
+    DC '2
+    DC '* | $80
+    DC.W F_WORD_PLUS
+    DC.W F_WORD_MUL_BY_2_CODE
+F_WORD_MUL_BY_2_CODE:
+    JSR PULL_FROM_S
+    ASL STACK_TMP
+    ROL STACK_TMP+1
+    JMP PUSH_TO_S
+
+F_WORD_NAND: ; NAND
+    DC 4  | $80
+    DC "NAN"
+    DC 'D | $80
+    DC.W F_WORD_MUL_BY_2
+    DC.W F_WORD_NAND_CODE
+F_WORD_NAND_CODE:
+    JSR PULL_FROM_S
+    COPY_WORD_TO STACK_TMP, INTERNAL_TMP
+    JSR PULL_FROM_S
+    LDA STACK_TMP
+    AND INTERNAL_TMP
+    EOR #$FF  ; for NOT
+    STA STACK_TMP
+    LDA STACK_TMP+1
+    AND INTERNAL_TMP+1
+    EOR #$FF  ; for NOT
+    STA STACK_TMP+1
+    JMP PUSH_TO_S
+
+F_WORD_EQUALS_0: ; 0=
+LAST_F_WORD:
+    DC 2  | $80
+    DC '0
+    DC '= | $80
+    DC.W F_WORD_NAND
+    DC.W F_WORD_EQUALS_0_CODE
+F_WORD_EQUALS_0_CODE:
+    SUBROUTINE
+    JSR PULL_FROM_S
+    LDA STACK_TMP
+    BNE .notZero
+    LDA STACK_TMP+1
+    BNE .notZero
+    JMP PUSH_TRUE_TO_S
+.notZero:
+    JMP PUSH_FALSE_TO_S
 
 IRQ:
     RTI
 
 TEXT:
-    dc "1 23 456 789A BCDE FFFF "
+    dc "1 1 0= 0 0 0= "
     dc 0
+
+
+
+IOBASE   = $8800
+IOSTATUS = IOBASE + 1
+IOCMD    = IOBASE + 2
+IOCTRL   = IOBASE + 3
+
+PRINT_STACK:
+    SUBROUTINE
+    LDA #$09
+    STA IOCMD      ; Set command status
+    LDA #$1A
+    STA IOCTRL     ; 0 stop bits, 8 bit word, 2400 baud
+
+.loop:
+    LDA SP_ADDR
+    CMP #<S0_CONST
+    BNE .print
+    LDA SP_ADDR+1
+    CMP #>S0_CONST
+    BEQ .halt
+.print:
+    JSR PULL_FROM_S
+    JSR PRINT_NUMBER
+    JMP .loop
+
+.halt:
+    BRK
+
+PRINT_NUMBER:
+    SUBROUTINE
+    LDX #4
+.loop:
+    LDY #4
+.shiftLoop:
+    ASL STACK_TMP
+    ROL STACK_TMP+1
+    ROL INTERNAL_TMP
+    DEY
+    BNE .shiftLoop
+    JSR PRINT_DIGIT
+    DEX
+    BNE .loop
+    LDA #$0D
+    JSR PRINT_CHAR
+    RTS
+
+PRINT_DIGIT:
+    SUBROUTINE
+    LDA INTERNAL_TMP
+    AND #$0F
+    CLC
+    CMP #10
+    BCC .lower
+    ADC #6
+.lower:
+    ADC #'0
+    STA $F4
+    JSR PRINT_CHAR
+    RTS
+
+PRINT_CHAR:
+    SUBROUTINE
+        PHA             ; Save accumulator
+.ECHO1:  LDA IOSTATUS    ; Read the ACIA status
+        AND #$10        ; Is the tx register empty?
+        BEQ .ECHO1       ; No, wait for it to empty
+        PLA             ; Otherwise, load saved accumulator,
+        STA IOBASE      ; write to output,
+        CMP #$0D        ; check if it was CR
+        BNE .END
+        LDA #$0A        ; then output LF too
+        PHA
+        JMP .ECHO1
+.END:    RTS             ; and return
+
 
 ; system vectors
 
--- figForth/prog/cr.sh
+++ figForth/prog/cr.sh
@@ -4,5 +4,5 @@
 
 set -e
 
-dasm $1 -f3
+dasm $1 -f3 -llist.txt
 java -Dsun.java2d.uiScale=2.0 -jar ~/Downloads/symon-1.4.0.jar -r a.out -b
