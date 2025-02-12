9b2205ea5bea3b6c818dea10c0befb86c2b372bc
Anton <strannik.anton@gmail.com>
2025-02-12 23:45:04 +0100

    Update uart6502 compile&run

--- uartConnector/pcPart/src/uart6502/libs/compileRunCommand.py
+++ uartConnector/pcPart/src/uart6502/libs/compileRunCommand.py
@@ -1,5 +1,6 @@
 import sys
 import subprocess
+import re
 
 from .writeCommand import run_write_cmd
 from .runCommand import run_run_cmd
@@ -13,7 +14,7 @@ def register_compile_and_run(subparsers):
 
 def run_compile_and_run_command(args):
     src = args.src
-    p = subprocess.run(['dasm', src, '-o/tmp/a.out', '-s/tmp/symbol.txt'], capture_output=True)
+    p = subprocess.run(['./build.py', src, '-o', '/tmp/a.out', '-m' ,'/tmp/map.txt'], capture_output=True)
     exit_code = p.returncode
     if exit_code != 0:
         print('Fail to compile the source')
@@ -26,14 +27,22 @@ def run_compile_and_run_command(args):
     run_write_cmd(args)
     run_run_cmd(args)
 
-
 def extract_main():
-    with open('/tmp/symbol.txt', 'r') as symbols:
-        for line in symbols:
-            if line.startswith('-') or len(line) == 0:
+    with open('/tmp/map.txt', 'r') as mapping:
+        pre_exports = True
+        for line in mapping.readlines():
+            if line.startswith('-----'):
                 continue
-            symbol_line = line.split()
-            if symbol_line[0].upper() == 'MAIN':
-                return symbol_line[1]
+            if pre_exports:
+                if line.startswith('Exports list by name:'):
+                    pre_exports = False
+            else:
+                if line == '\n':
+                    break
+                label_line = re.split(r"\s+", line)
+                if label_line[0].upper() == "MAIN":
+                    return label_line[1]
+                if len(label_line) > 3 and label_line[3].upper() == "MAIN":
+                    return label_line[4]
     print("No MAIN label in the source")
     sys.exit(1)
